50919ce231151f4e17e7a594ff1d2eec6720cce8
Guliaiev Andrey <andrguliaiev@gmail.com>
2021-04-08 11:04:17 +0300

    lombok3

--- src/main/java/ru/netologi/domain/Radio.java
+++ src/main/java/ru/netologi/domain/Radio.java
@@ -1,11 +1,11 @@
 package ru.netologi.domain;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-@Data
+
 @AllArgsConstructor
 public class Radio {
 
